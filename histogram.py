3990e84cef2e46df72787fcb312d50bd4157a4b9
Anand Sadasivan <139990619+Anand-29@users.noreply.github.com>
2024-02-23 16:25:57 +0530

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -1,19 +1,23 @@
-list=[2,3,5,2,1]
-lb=[0]*5;rb=[0]*5
-c=0
-for i in range(0,5):
+list=[1,2,3,4,3,1,5]
+lb=[0]*len(list);rb=[0]*len(list);c=0
+for i in range(0,len(list)-1):
     c=0
-    for j in range(i+1,5):
-        if(list[j]>=list[i]):
-            c+=1    
-    rb[i]=c
+    for j in range(i+1,len(list)):
+        if(list[i]<=list[j]):
+            c+=1
+        else:
+            break
+    lb[i]=c
     c=0
     for k in range(i-1,-1,-1):
-        if(list[k]>=list[i]):
-            c+=1    
-    lb[i]=c
-max=0
-for i in range(0,5):
-    if((lb[i]+rb[i]+1)*list[i]>=max):
+        if(list[i]<=list[k]):
+            c+=1
+        else:
+            break
+    rb[i]=c
+    max=0
+for i in range(0,len(list)):
+    if((lb[i]+rb[i]+1)*list[i])>=max:
         max=(lb[i]+rb[i]+1)*list[i]
 print(max)
+    
